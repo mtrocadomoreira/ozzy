0353392a2d21e3a92d025049273389122b019896
Mariana Moreira <mtrocadomoreira@gmail.com>
2023-12-13 11:59:18 +0100

    work on function to get mean and rms from raw data

--- statistics.py
+++ statistics.py
@@ -33,17 +33,48 @@ def mean_rms_raw(xds, dims, binned_axes, savepath=os.getcwd(), outfile=None, exp
     if problem > 0:
         raise Exception('Problem matching output dim(s) with input dim(s).')
 
-    # Search for time dimension in input array
-    # - not sure if this is necessary
-
     # Prepare binning array
-    # for data
 
-    if isinstance(binned_axes, xr.DataArray)
+    bin_lims = []
+    bin_vars = []
+    bin_axes = []
+    if isinstance(binned_axes, xr.DataArray):
+        axis = np.array(binned_axes)
+        bin_axes[0] = axis
 
-    bin_arr = 
+        axis = axis + 0.5*(axis[1]-axis[0])
+        vmin = axis[0] - 0.5*(axis[1]-axis[0])
+        axis = np.insert(axis, 0, vmin)
+        
+        bin_arr[0] = axis
+        bin_var[0] = binned_axes.name
+    elif isinstance(binned_axes, xr.Dataset):
+        for var in binned_axes.data_vars:
+            axis = np.array(binned_axes[var])
+            bin_axes.append(axis)
 
-    newarr = np.empty
+            vmin = axis[0] - 0.5*(axis[1]-axis[0])
+            axis = axis + 0.5*(axis[1]-axis[0])
+            axis = np.insert(axis, 0, vmin)
+
+            bin_arr.append(axis)
+            bin_var.append(var)
+    else:
+       raise Exception('Error: Was expecting the keyword "binned_axes" to be either an xarray.DataArray or an xarray.Dataset.')
+
+    # Prepare dataset for calculation
+
+    for dim in dims:
+        xds[dim+'_sqr'] = xds[dim]**2
+        xds[dim+'_sqrw'] = xds[dim+'_sqr'].weighted(xds['q'])
+    
+    ds = xds[bin_var + [dim+'_sqrw' for dim in dims]]
+
+    # Group into bins along each binnable dimension
+
+    for bvar, bax in zip(bin_var, bin_arr):
+
+
     
 
     # name of file: 'quant_rms_grid.pkl' or keyword
