8b3c3adc1093c120c7449c9fd7fb4a1a170bc269
Mariana Moreira <mtrocadomoreira@gmail.com>
2024-02-26 19:16:09 +0100

    work on ave. phase velocity diagnostic

--- ozzy/fields.py
+++ ozzy/fields.py
@@ -6,20 +6,56 @@ import time
 
 # --- Helper functions ----
 
-
+def get_kaxis(axis):
+    nx = axis.size
+    dx = (axis[-1]-axis[0]) / nx
+    kaxis = np.fft.fftshift(np.fft.fftfreq(nx,dx))
+    return kaxis
 
 # --- Diagnostics ---
 
-def ave_vphi_from_waterfall():
+def ave_vphi_from_waterfall(da, dcells, xvar='zeta', yvar='t'):
 
-    # set up rolling window
+    # define dcells in each direction: dx, dt
 
-    # for each region:
-        # take 2D fft of waterfall data
+    # check if dcells in either direction is odd (and correct if not)
 
-        # calculate 2d vphi from fft
-        # normalize
-        # sum entire region
-        # save value in new grid
+    # define margins
+
+    mx = floor(dx * 0.5)
+    mt = floor(dt * 0.5)
+
+    data = da.to_numpy()
+    vphi = np.zeros_like(data)
+    Nt, Nx = data.shape
+
+    xax = data.coords[xvar].to_numpy()
+    tax = data.coords[yvar].to_numpy()
+
+    # Define vphi map for each subwindow
+
+    kx = get_kaxis(xax[0:dx])
+    kt = get_kaxis(tax[0:dt])
+    Kx, Kt = np.meshgrid(kx,kt)
+    vphi_map = 1.0 - Ky / Kx
+    vphi_map[np.where(Kx == 0)] = 0
+
+    # Loop along center of data
+
+    for i in np.arange(mx, Nx-mx):
+        for j in np.arange(mt, Nt-mt):
+
+            window = data[j-mt:j+mt+1, i-mx:i+mx+1]
+
+            fftdata = abs(np.fft.fftshift( np.fft.fft2(window) ))
+            factor = np.nansum(fftdata)
+            fftdata = fftdata/factor
+
+            vphi[j,i] = np.sum(fftdata*vphi)
+
+    # Deal with margins
+
+
+
 
     return
